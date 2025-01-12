40f62cc671a79b774154a028f06dfe229c95399e
Mateusz <trynio132@gmail.com>
2025-01-12 21:46:14 +0100

    first-components

---
+++ frontend/vue-project/src/views/CartView.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: 'CartView',
+};
+</script>
+
+<template>
+  <v-container class="text-center">
+    <h1>Twój koszyk</h1>
+    <p>Wkrótce będziesz mógł przejrzeć swoje produkty i złożyć zamówienie.</p>
+  </v-container>
+</template>
