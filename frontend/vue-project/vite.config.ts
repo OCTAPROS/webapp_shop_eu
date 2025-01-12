6c27fcd97ae8f19e615ab3102700645901883614
Mateusz <trynio132@gmail.com>
2025-01-12 21:05:19 +0100

    fix fronted container

--- frontend/vue-project/vite.config.ts
+++ frontend/vue-project/vite.config.ts
@@ -19,9 +19,9 @@ export default defineConfig({
   },
   server: {
     host: "0.0.0.0",
-    port: 80,
+    port: 5173,
     hmr: {
-      clientPort: 80,
+      clientPort: 5173,
     },
     watch: {
       usePolling: true,
