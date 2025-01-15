5f597d04db0788bf1b689b06fe55a0422847d806
Mateusz <40675173+MateuszTryniszewski@users.noreply.github.com>
2025-01-15 20:20:21 +0100

    first-components (#8)

--- frontend/vue-project/src/App.vue
+++ frontend/vue-project/src/App.vue
@@ -1,87 +1,26 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+<v-app>
+  <v-app-bar app color="primary" dark>
+    <v-toolbar-title>Sklep Online</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn to="/">Strona główna</v-btn>
+    <v-btn to="/products">Produkty</v-btn>
+    <v-btn to="/cart">Koszyk</v-btn>
+  </v-app-bar>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <v-main>
+    <router-view></router-view>
+  </v-main>
+</v-app>
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-      <v-otp-input></v-otp-input>
-      <v-date-picker></v-date-picker>
-    </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.text {
+  font-size: large;
 }
 </style>
--- frontend/vue-project/src/views/HomeView.vue
+++ frontend/vue-project/src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+
 </script>
 
 <template>
   <main>
     <TheWelcome />
+    <v-container class="text-center">
+    <h1>Witamy w naszym sklepie online!</h1>
+    <p>Oferujemy szeroki wybór produktów w atrakcyjnych cenach.</p>
+  </v-container>
   </main>
 </template>
