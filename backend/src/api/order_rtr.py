3554c467562d00e0633fe7a3917d62b85230c9ea
mpasich_octp <mariusz.pasich1@gmail.com>
2025-02-23 14:36:12 +0100

    mp

--- backend/src/api/order_rtr.py
+++ backend/src/api/order_rtr.py
@@ -10,8 +10,7 @@ from models.user import User
 router = APIRouter()
 
 @router.get("/", response_model=List[OrderAdminView])
-# def get_all_orders(skip: int = 0, limit = 10, db_session: Session = Depends(get_session), current_user: User = Depends(get_current_user)):
-def get_all_orders(skip: int = 0, limit = 10, db_session: Session = Depends(get_session)):
+def get_all_orders(skip: int = 0, limit = 10, db_session: Session = Depends(get_session), current_user: User = Depends(get_current_user)):
     objects = db_session.exec(select(OrderAdminView).offset(skip).limit(limit)).all()
     if not objects:
         raise HTTPException(detail=f"There are no Orders for selected criteria", status_code=status.HTTP_404_NOT_FOUND)
@@ -27,21 +26,25 @@ def get_all_orders(skip: int = 0, limit = 10, db_session: Session = Depends(get_
 #     # print(order.order_rows.__dict__)
 #     return order
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=Order, status_code=status.HTTP_201_CREATED)
 def create_order(order_data: OrderInsert, 
-                 db_session: Session = Depends(get_session) #,
-                #  current_user: User = Depends(get_current_user)
+                 db_session: Session = Depends(get_session),
+                 current_user: User = Depends(get_current_user)
                  ):
     print("##############################")
     print(order_data.__dict__)
+    print("##############################")
+    order: Order = Order(**order_data.model_dump())
+
+    db_session.add(order)  
+    db_session.commit()
+    db_session.refresh(order)
+
+    print("##############################2")
+    print(order.__dict__)
     print("##############################2")
 
-    # for or1 in order.order_rows:
-    #     print(or1)
-    # # db_session.add(order)  
-    # # db_session.commit()
-    # # db_session.refresh(order)
-    return order_data
+    return order
 
 # @router.put("/{id}", response_model=Order, status_code=status.HTTP_200_OK)
 # def update_order(id:int, product_data: Order, db_session: Session = Depends(get_session)):
